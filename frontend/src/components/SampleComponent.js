36b3e0f131bbf3840ed06e4f979fa54a70eea08b
specialOne16 <jundi2303@gmail.com>
2021-02-09 15:37:42 +0700

    set component structure

---
+++ frontend/src/components/SampleComponent.js
@@ -0,0 +1,11 @@
+import './SampleComponent.css';
+
+function SampleComponent() {
+  return (
+    <div className="title">
+      Hello World!    
+    </div>
+  );
+}
+
+export default SampleComponent;
