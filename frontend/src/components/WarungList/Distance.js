08e32eeb72ec179a58d5cb5634bacfb4ad517183
Raras Pradnya <rarasprdny@gmail.com>
2021-03-02 08:53:10 +0700

    Cleanup

--- frontend/src/components/WarungList/Distance.js
+++ frontend/src/components/WarungList/Distance.js
@@ -14,24 +14,6 @@ class Distance extends Component{
         };
       };
 
-    // countDistance(){
-    //     const R = 6371; // Radius of the earth in km
-    //     const lat1 = this.state.latitude;
-    //     const lon1 = this.state.longitude;
-    //     const lat2 = this.props.latitude;
-    //     const lon2 = this.props.longitude;
-        
-    //     const dLat = (lat2-lat1) * Math.PI / 180;  // Javascript functions in radians
-    //     const dLon = (lon2-lon1) * Math.PI / 180; 
-    //     const a = Math.sin(dLat/2) * Math.sin(dLat/2) +Math.cos(lat1* Math.PI / 180) * Math.cos(lat2* Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2); 
-    //     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    //     this.state.dist = R * c; // Distance in km  
-    //     console.log(lat1);
-    //     console.log(lon1);
-    //     console.log(lat2);
-    //     console.log(lon1);
-    // };
-
     componentDidMount() {
         navigator.geolocation.getCurrentPosition(
             (position) => {
