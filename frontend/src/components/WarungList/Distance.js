9f4980d4640f7f9e6e7e8d9acd6ef840e739464e
Raras Pradnya <rarasprdny@gmail.com>
2021-03-02 08:56:18 +0700

    Clean

--- frontend/src/components/WarungList/Distance.js
+++ frontend/src/components/WarungList/Distance.js
@@ -28,10 +28,6 @@ class Distance extends Component{
                 const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1* (Math.PI / 180)) * Math.cos(lat2* (Math.PI / 180)) * Math.sin(dLon/2) * Math.sin(dLon/2); 
                 const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
                 const d = Number((R *c ).toFixed(1)); // Distance in km  
-                console.log(lat1);
-                console.log(lon1);
-                console.log(lat2);
-                console.log(lon2);
                 this.setState({
                     latitude: position.coords.latitude,
                     longitude: position.coords.longitude,
@@ -42,8 +38,6 @@ class Distance extends Component{
             (error) => this.setState({ error: error.message }),
             { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 },
         )
-        // this.countDistance();
-        
     };
 
     render(){
