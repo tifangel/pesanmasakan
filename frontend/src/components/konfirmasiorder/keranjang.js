56dcf23e99b53a90014e91081850aa3552dab826
Jundullah <13518027@std.stei.itb.ac.id>
2021-04-04 12:01:32 +0700

    add keranjang functionality

--- frontend/src/components/konfirmasiorder/keranjang.js
+++ frontend/src/components/konfirmasiorder/keranjang.js
@@ -1,78 +1,209 @@
 import React, { useState } from "react";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, Divider, Typography } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+
+const { defaultAPIURL } = require("../../config");
 
 function preventDefault(event) {
   event.preventDefault();
 }
 
+function formatMoney(money){
+    if(money>=1000){
+        return `Rp${Math.floor(money/1000)}.${
+            (money%1000<10)? `00${money%1000}` : (money%1000<100)? `0${money%1000}` : money%1000 
+        }`;
+    }else{
+        return `Rp${money}`;
+    }
+}
+
 const useStyles = makeStyles((theme) => ({
   summary: {
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(1),
   },
   button: {
     width: "100%",
+    marginTop: theme.spacing(1),
+    color: "#FFF",
+    backgroundColor: "#FDCB35",
+    "&:hover": {
+      color: "#FFF",
+      backgroundColor: "#FDCB35",
+    },
   },
-  tablecell:{
-      borderBottom: "none"
-  }
+  tablecell: {
+    borderBottom: "none",
+    fontWeight: 500,
+  },
+  menuimg: {
+    objectFit: "contain",
+    width: 80,
+    height: 80,
+  },
+  menuroot: {
+    display: "flex",
+    justifyContent: "center",
+  },
 }));
 
 export default function Keranjang() {
   const classes = useStyles();
   const [menuList, setMenuList] = useState([
     {
-      nama: "Sop",
+      id: 1,
+      nama: "Sup Ayam Kepiting Jagung",
       harga: 10000,
+      jumlah: 2,
     },
     {
-      nama: "Sop",
+      id: 2,
+      nama: "Sup Ayam Kepiting Jagung",
       harga: 10000,
+      jumlah: 2,
     },
     {
-      nama: "Sop",
-      harga: 10000,
+      id: 3,
+      nama: "Sup Ayam Kepiting Jagung",
+      harga: 55000,
+      jumlah: 3,
     },
   ]);
+  const [ongkir, setOngkir] = useState(10000);
+  const [subtotal, setSubtotal] = useState(10000);
+
+  const updateMenu = (id, jumlah) => {
+    setMenuList(
+      menuList
+        .map((it) => {
+          if (it.id === id) it.jumlah = Math.min(Math.max(jumlah, 0), 9);
+          return it;
+        })
+        .filter((it) => it.jumlah > 0)
+    );
+    setSubtotal(menuList.reduce((total, it) => total+it.harga*it.jumlah, 0));
+  };
+
   return (
     <React.Fragment>
-      <Typography component="h2" variant="h6" color="primary" gutterBottom>
+      <Typography
+        component="h2"
+        variant="h6"
+        color="primary"
+        gutterBottom
+        style={{ color: "#000" }}
+      >
         Keranjang Anda
       </Typography>
-      <Divider />
+      <Divider style={{ marginTop: 10, marginBottom: 10 }} />
       {menuList.map((it) => (
         <React.Fragment>
-          <img alt="menupic" />
-          <span>{it.nama}</span>
-          -2+
-          <span>Rp{it.harga}</span>
-          <Divider />
+          <Card elevation="0dp" className={classes.menuroot}>
+            <CardMedia
+              image={`${defaultAPIURL}/menu/1-ayam-goreng.jpg`}
+              alt="menupic"
+              className={classes.menuimg}
+            />
+            <CardContent
+              style={{
+                paddingRight: 0,
+                paddingLeft: 0,
+                width: 120,
+                marginLeft: 12,
+              }}
+            >
+              <Typography display="inline" style={{ fontWeight: 500 }}>
+                {it.nama}
+              </Typography>
+            </CardContent>
+            <CardContent
+              style={{ paddingRight: 0, paddingLeft: 0 }}
+            >
+              <IconButton
+                size="small"
+                onClick={() => updateMenu(it.id, it.jumlah - 1)}
+              >
+                <RemoveIcon />
+              </IconButton>
+              {it.jumlah}
+              <IconButton
+                size="small"
+                onClick={() => updateMenu(it.id, it.jumlah + 1)}
+              >
+                <AddIcon />
+              </IconButton>
+              <Typography display="inline" style={{ fontWeight: 500 }}>
+                {formatMoney(it.harga)}
+              </Typography>
+            </CardContent>
+          </Card>
+          <Divider style={{ marginTop: 10, marginBottom: 10 }} />
         </React.Fragment>
       ))}
       <div className={classes.summary}>
-      <Table className={classes.table}>
-        <TableBody>
+        <Table className={classes.table}>
+          <TableBody>
             <TableRow>
-              <TableCell className={classes.tablecell} padding="none" >Subtotal</TableCell>
-              <TableCell className={classes.tablecell} padding="none" align="right">Rp30.000</TableCell>
+              <TableCell className={classes.tablecell} padding="none">
+                Subtotal
+              </TableCell>
+              <TableCell
+                className={classes.tablecell}
+                padding="none"
+                align="right"
+              >
+              {formatMoney(subtotal)}
+              </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className={classes.tablecell} padding="none" >Delivery Fee</TableCell>
-              <TableCell className={classes.tablecell} padding="none" align="right">Rp30.000</TableCell>
+              <TableCell className={classes.tablecell} padding="none">
+                Delivery Fee
+              </TableCell>
+              <TableCell
+                className={classes.tablecell}
+                padding="none"
+                align="right"
+              >
+              {formatMoney(ongkir)}
+              </TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className={classes.tablecell} padding="none" >Total</TableCell>
-              <TableCell className={classes.tablecell} padding="none" align="right">Rp30.000</TableCell>
+              <TableCell className={classes.tablecell} padding="none">
+                Total
+              </TableCell>
+              <TableCell
+                className={classes.tablecell}
+                padding="none"
+                align="right"
+              >
+                {formatMoney(subtotal+ongkir)}
+              </TableCell>
             </TableRow>
-        </TableBody>
-      </Table>
-        <Button color="primary" href="#" onClick={preventDefault} className={classes.button}>
+          </TableBody>
+        </Table>
+        <Button
+          color="primary"
+          href="#"
+          onClick={preventDefault}
+          className={classes.button}
+        >
           Bayar
         </Button>
       </div>
