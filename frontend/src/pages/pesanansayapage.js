b0ff42ba3a5d8ced603c2ac723a4d3e6656c1a22
tifangel <tifanyangelia28@gmail.com>
2021-04-10 15:31:13 +0700

    fix integrasi pesanan

--- frontend/src/pages/pesanansayapage.js
+++ frontend/src/pages/pesanansayapage.js
@@ -127,7 +127,19 @@ function columns(props) {
       headerClassName: "header-style",
       valueFormatter: ({ value }) => {
         const time = new Date(value);
-        return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} WIB`;
+        return `
+          ${
+            time.getHours() < 10 ? `0${time.getHours()}` : time.getHours()
+          }
+          :
+          ${
+            time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
+          }
+          :
+          ${
+            time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds()
+          } 
+          WIB`;
       },
     },
     {
