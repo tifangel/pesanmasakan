3ad743c103c901f88abbadf358f88c7e02323bdc
Jundullah <13518027@std.stei.itb.ac.id>
2021-04-10 08:41:16 +0700

    add header component and add in every page

--- frontend/src/pages/infopage.js
+++ frontend/src/pages/infopage.js
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import "./styleinfo.css";
-import InfoTambahan from "../components/InfoTambahan";
+import AppHeader from "../components/header";
 import { getWarung, getMenuListByWarungId, getDaysbyMenuId } from "../resource";
 import IconButton from "@material-ui/core/IconButton";
 import StarIcon from "@material-ui/icons/Star";
@@ -230,7 +230,7 @@ const InfoPage = (props) => {
 
   return (
     <React.Fragment>
-      <InfoTambahan />
+      <AppHeader />
       <div className={classes.root}>
         <div
           style={{
