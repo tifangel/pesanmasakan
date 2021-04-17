b8945d9bbbcb9ea2bea9c70348a42baebb175d40
Jundullah <13518027@std.stei.itb.ac.id>
2021-04-17 16:37:03 +0700

    change component style based on innerwidth

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -73,7 +73,7 @@ const HomePage = (props) => {
     return(
         <div>
             <AppHeader />
-            <InfoTambahan />
+            {(window.innerWidth > 768)? <InfoTambahan /> : null}
             <Searchbar/>
             <WarungList data={warungList}/>
         </div>
